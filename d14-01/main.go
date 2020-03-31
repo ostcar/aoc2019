4d4c6ccb31b5619f3526d089023a693d1dde1043
Oskar Hahn <mail@oshahn.de>
2020-03-31 22:56:40 +0200

    Day 14

---
+++ d14-01/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Can not open input: %v", err)
+	}
+	defer f.Close()
+
+	fmt.Println(calcFuel(f))
+}
+
+func calcFuel(r io.Reader) int {
+	reaktions := readReactions(r)
+	buffer := make(map[string]int)
+	v := calcOres(reaktions["FUEL"], reaktions, buffer)
+	fmt.Println(buffer)
+	return v
+}
+
+// calcOres returns the required ores for an reaction
+func calcOres(r *reaktion, reaktions map[string]*reaktion, buffer map[string]int) int {
+	if len(r.input) == 1 && r.input[0].name == "ORE" {
+		return r.input[0].amount
+	}
+
+	var count int
+	for _, input := range r.input {
+		if buffer[input.name] >= input.amount {
+			buffer[input.name] -= input.amount
+			continue
+		}
+
+		needed := input.amount - buffer[input.name]
+		for needed > 0 {
+			count += calcOres(reaktions[input.name], reaktions, buffer)
+			needed -= reaktions[input.name].output.amount
+		}
+		buffer[input.name] = -needed
+	}
+	return count
+}
+
+func readReactions(r io.Reader) map[string]*reaktion {
+	scanner := bufio.NewScanner(r)
+	reaktions := make(map[string]*reaktion)
+	for scanner.Scan() {
+		r := newReaktion(scanner.Text())
+		reaktions[r.output.name] = r
+
+	}
+	if scanner.Err() != nil {
+		log.Fatalf("Can not read from input: %v", scanner.Err())
+	}
+	return reaktions
+}
+
+type typeAmount struct {
+	name   string
+	amount int
+}
+
+func newTypeAmount(s string) typeAmount {
+	var ta typeAmount
+	v := strings.Split(strings.TrimSpace(s), " ")
+	a, err := strconv.Atoi(strings.TrimSpace(v[0]))
+	if err != nil {
+		log.Fatalf("can not convert typeAmout value `%s`", s)
+	}
+	ta.amount = a
+	ta.name = strings.TrimSpace(v[1])
+	return ta
+}
+
+func (s typeAmount) String() string {
+	return fmt.Sprintf("%d %s", s.amount, s.name)
+}
+
+type reaktion struct {
+	output typeAmount
+	input  []typeAmount
+}
+
+func newReaktion(s string) *reaktion {
+	r := reaktion{}
+	inputOutout := strings.Split(s, "=>")
+
+	inputElements := strings.Split(inputOutout[0], ",")
+	for _, e := range inputElements {
+		r.input = append(r.input, newTypeAmount(e))
+	}
+	r.output = newTypeAmount(inputOutout[1])
+	return &r
+}
+
+func (r *reaktion) String() string {
+	buf := strings.Builder{}
+
+	var inputs []string
+	for _, input := range r.input {
+		inputs = append(inputs, input.String())
+	}
+	buf.WriteString(strings.Join(inputs, ", "))
+	buf.WriteString(" => ")
+	buf.WriteString(r.output.String())
+	buf.WriteByte('\n')
+	return buf.String()
+}
